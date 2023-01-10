d4c64e8a773cbf343583ce2bebc9563b07cf007e
Miles Crane <miles.crane@stu.barren.kyschools.us>
2023-01-10 10:40:10 -0600

    completed JSBasicsExercise and bonus

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log(4);
+
+/*
+PRIMITIVES
+    number
+    string
+    boolean
+    null
+    undefined
+*/
+
+/* 
+OPERATORS
+    +   addition
+    -   subtraction
+    *   multiplication
+    /   division
+    **  exponentation
+    %   modulus
+*/
+
+console.log(5 % 2);
+console.log(5 % 4);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye");
+
+console.log("Miles".length + "Crane".length);
+
+console.log("Avengers: Endgame"[16]);
+
+// bonus
+console.log("\\");
+
+console.log(5 ** 4 - 9);
+
+console.log("I".length + "Am".length % "Iron".length - "Man".length);
+
+console.log("CAP"[1]); 
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+var m = 25;
+var c = 4;
+var u = (m + c);
+console.log(m, c, u);
